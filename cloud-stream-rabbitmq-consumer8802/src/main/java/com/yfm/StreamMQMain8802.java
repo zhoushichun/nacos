4812f3ba470aa92237818e56d4e04ae151de85a9
Administrator <646300036@qq.com>
2021-04-23 23:29:13 +0800

    晚上

--- cloud-stream-rabbitmq-consumer8802/src/main/java/com/yfm/StreamMQMain8802.java
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/yfm/StreamMQMain8802.java
@@ -6,8 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class StreamMQMain8802
-{
+public class StreamMQMain8802 {
     public static void main(String[] args)
     {
         SpringApplication.run(StreamMQMain8802.class,args);
